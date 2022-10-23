47f937ed82847ca09a2646c3917a8f3fe962e47c
codeducksof <rattanapong.teeranaew@gmail.com>
2022-10-24 00:06:35 +0700

    Creating Bottom Navigation Bar
    สร้าง ปุ่ม 3 ปุ่ม โดยจะมี ปฏิฑิน เช็คอิน และ โปรไฟล์

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,6 +1,8 @@
+import 'package:basicflutter2/calendarscreen.dart';
+import 'package:basicflutter2/profilescreen.dart';
+import 'package:basicflutter2/todayscreen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,11 +12,97 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double screenHeight = 0;
+  double screenWidth = 0;
+
+  Color primary = const Color(0xFFEEF444C);
+  int currentIndex = 0;
+
+  List<IconData> navigationIcons = [
+    FontAwesomeIcons.calendarDays,
+    FontAwesomeIcons.check,
+    FontAwesomeIcons.userLarge,
+  ];
+
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Center(
-        child: Text("Home"),
+      body: IndexedStack(
+        index: currentIndex,
+        children: const [
+          //3 ไฟล์นี้จะอ้างอิงจาก setState currentIndex
+          CalendarScreen(),
+          TodayScreen(),
+          ProfileScreen(),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        height: 70,
+        margin: const EdgeInsets.only(
+          left: 12,
+          right: 12,
+          bottom: 24,
+        ),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, blurRadius: 10, offset: Offset(2, 2))
+            ]),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(40)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < navigationIcons.length; i++) ...<Expanded>{
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      //const
+                      setState(() {
+                        currentIndex = i;
+                      });
+                    },
+                    child: Container(
+                      height: screenHeight,
+                      width: screenWidth,
+                      color: Colors.white,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              navigationIcons[i],
+                              color:
+                                  i == currentIndex ? primary : Colors.black54,
+                              size: i == currentIndex ? 30 : 26,
+                            ),
+                            i == currentIndex
+                                ? Container(
+                                    margin: const EdgeInsets.only(top: 6),
+                                    height: 3,
+                                    width: 22,
+                                    decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.all(
+                                            Radius.circular(40)),
+                                        color: primary),
+                                  )
+                                : const SizedBox(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              }
+            ],
+          ),
+        ),
       ),
     );
   }
